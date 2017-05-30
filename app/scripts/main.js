71bae8040c82ebcb7ce1aa732f54313ea84616b5
Henry David Richardson, Jr <hrichardson@stackframe.com>
2017-05-29 23:58:43 -0400

    Checking out easelJS for viability

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+function init() {
+	var stage = new createjs.Stage("mainGame");
+
+	var circle = new createjs.Shape();
+	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+	circle.x = 100;
+	circle.y = 100;
+	stage.addChild(circle);
+
+	stage.update();
+
+	var moon = new createjs.Shape()
+	moon.graphics.beginFill("silver").drawCircle(0, 0, 50);
+	moon.x = 400;
+	moon.y = 150;
+	stage.addChild(moon);
+
+	stage.update();
+	circle.x = circle.x + 3;
+	while(circle.x<moon.x) {
+		circle.x = circle.x + 20;
+		stage.update();
+	}
+}
+
+init();
